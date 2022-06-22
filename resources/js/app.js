8aa08ea2c5f0dd339a04c883e87e691a4172ec17
githubBram01 <93133849+githubBram01@users.noreply.github.com>
2022-06-22 11:28:43 +0200

    mobile reponsiveness navbar close + styling

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,11 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
+let narbartoggler = document.querySelector('.navbar-toggler');
+let navbarmobileicon =  document.querySelector('.navbar-toggler-icon');
+narbartoggler.onclick = function(){
+    navbarmobileicon.classList.toggle("navbar-open");
+};
 
 
 
